cfdfadc383f888c0577689a7dbd8e238cff67908
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-08-01 15:31:56 +0200

    add box shadow custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        full: "4px 6px black",
+      },
       colors: {
         peach: "#FEF2E8",
         yellow: "#FFE959",

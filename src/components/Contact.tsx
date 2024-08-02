ad9569a1bb2fcb4538ae8048c393a7a6b85424d2
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-08-02 16:18:37 +0200

    add responsive version to contact part

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,14 +7,21 @@ import SvgArrow from "./svg/SvgArrow"
 
 const Contact = () => {
   return (
-    <div className="relative flex flex-col items-center">
+    <div className="relative flex flex-col items-center sm:items-start">
       <h2 className="mt-16 font-syne text-xl font-bold">Me contacter</h2>
       <p className="mt-4">
         Oh woaw ! Vous avez lu jusqu’ici ? Merci ! Si vous souhaitez prendre
         contact :
       </p>
-      <Button className="mb-20 mt-6" type="button" text="Me contacter" />
-      <SvgArrow size="90" className="absolute bottom-0 left-1/4" />
+      <Button
+        className="mb-20 mt-6 sm:self-end"
+        type="button"
+        text="Me contacter"
+      />
+      <SvgArrow
+        size="90"
+        className="absolute bottom-0 max-sm:left-1/4 sm:right-1/4"
+      />
     </div>
   )
 }

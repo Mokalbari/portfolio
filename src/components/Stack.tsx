95387bd1249fddc3dcf37c2fe0f97f912a3acdb3
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-08-02 20:23:29 +0200

    add grid pattern to background

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -16,23 +16,31 @@ const Stack = () => {
 
   return (
     <section className="mt-14">
-      <h2 className="text-center font-syne text-xl font-bold">
+      <h2 className="text-center font-syne text-xl font-bold sm:text-left">
         Ma stack technique
       </h2>
-      <h3 className="text-center font-medium">
+      <h3 className="text-center font-medium sm:text-left">
         React | TypeScript | ExpressJS | MySQL
       </h3>
-      <StackCard
-        className="mt-8 bg-yellow"
-        title="Frontend"
-        skills={frontendSkills}
-      />
-      <StackCard
-        className="mt-4 bg-yellow"
-        title="Backend"
-        skills={backendSkills}
-      />
-      <StackCard className="mt-4 bg-blue" title="Autres" skills={otherSkills} />
+      <div className="sm:mt-8 sm:flex sm:gap-4">
+        <StackCard
+          className="mt-8 bg-yellow text-left sm:mt-0 sm:w-2/5"
+          title="Frontend"
+          skills={frontendSkills}
+        />
+        <div className="sm:flex-1">
+          <StackCard
+            className="mt-4 bg-yellow sm:mb-4 sm:mt-0"
+            title="Backend"
+            skills={backendSkills}
+          />
+          <StackCard
+            className="mt-4 bg-blue sm:mt-0"
+            title="Autres"
+            skills={otherSkills}
+          />
+        </div>
+      </div>
     </section>
   )
 }
